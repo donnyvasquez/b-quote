329dc3848abb5341569fab573e26f208a1b91056
DONNY <dvasquez@donnyvasquez.com>
2024-08-30 10:22:39 -0500

    :wrench: Ajustes Selector de más planes

--- src/app/bupa-ion-radio/bupa-ion-radio.component.ts
+++ src/app/bupa-ion-radio/bupa-ion-radio.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +12,8 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class BupaIonRadioComponent {
+  @Output() optionSelected = new EventEmitter<string>();
+
   @Input() options: { label: string; description: string; value: string }[]  | undefined;
   @Input() groupLabel: string = '';
   @Input() singleLabel: string = 'Opción única';
@@ -21,12 +23,13 @@ export class BupaIonRadioComponent {
   selectRadio(event: Event, option: any) {
     const target = event.target as HTMLElement;
 
-    // Verifica si el clic fue en un ion-radio o en un ion-item
-    if (target.tagName === 'ION-RADIO' || target.tagName === 'ION-ITEM') {
-      return;
-    }
+      // Verifica si el clic fue en un ion-radio o en un ion-item
+      if (target.tagName === 'ION-RADIO' || target.tagName === 'ION-ITEM') {
+        return;
+      }
 
     // Cambia el valor seleccionado si el clic no fue en un ion-radio
     this.selectedValue = option.value;
+    this.optionSelected.emit(this.selectedValue);
   }
 }
--- src/app/international-business-plans/international-business-plans.component.ts
+++ src/app/international-business-plans/international-business-plans.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ButtonActionsComponent } from "../button-actions/button-actions.component";
@@ -15,7 +15,7 @@ import { BupaIonRadioComponent } from "../bupa-ion-radio/bupa-ion-radio.componen
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class InternationalBusinessPlansComponent  implements OnInit {
-
+  @ViewChild('moreOptionsAnchor') moreOptionsAnchor: ElementRef | undefined;
   public selectedCount: number = 0;
   public plans = [
     {
@@ -78,6 +78,129 @@ export class InternationalBusinessPlansComponent  implements OnInit {
     }
   ];
 
+  public morePlans1 = [
+    {
+      planId: '4',
+      planTitle: 'Bupa Advantage Care 1',
+      planSubtitle: 'Cobertura mínima',
+      planDescription: 'Este plan ofrece una cobertura básica para tu vehículo.',
+      planPrice: 120,
+      planMountLimit: 'Limite máximo: 3.5 millones',
+      planCoverage: 'Cobertura: Latinoamérica, el Caribe y EE.UU.',
+      planPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      deductibleMount: ['Dentro del País: USD 500', 'Fuera del País: USD 1,000'],
+      promoPercents: '15%',
+      promoPlanPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      validDate: 'Válido: febrero  1 - abril 30',
+      promoPlanPrice: 102,
+      aditionalCoverageOptions: [
+        { icon: 'heart-outline', label: 'Maternidad' },
+        { icon: 'airplane-outline', label: 'Piloto' }
+      ]
+    },
+    {
+      planId: '5',
+      planTitle: 'Bupa Advantage Care 2',
+      planSubtitle: 'Cobertura mínima',
+      planDescription: 'Este plan ofrece una cobertura básica para tu vehículo.',
+      planPrice: 120.00,
+      planMountLimit: 'Limite máximo: 3.5 millones',
+      planCoverage: 'Cobertura: Latinoamérica, el Caribe y EE.UU.',
+      planPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      deductibleMount: ['Dentro del País: USD 500', 'Fuera del País: USD 1,000'],
+      promoPercents: '15%',
+      promoPlanPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      validDate: 'Válido: febrero  1 - abril 30',
+      promoPlanPrice: 102,
+      aditionalCoverageOptions: [
+        { icon: 'medkit-outline', label: 'Transplante de órganos' },
+        { icon: 'heart-outline', label: 'Maternidad' },
+        { icon: 'airplane-outline', label: 'Piloto' }
+      ]
+    },
+    {
+      planId: '6',
+      planTitle: 'Bupa Advantage Care 3',
+      planSubtitle: 'Cobertura mínima',
+      planDescription: 'Este plan ofrece una cobertura básica para tu vehículo.',
+      planPrice: 360,
+      planMountLimit: 'Limite máximo: 3.5 millones',
+      planCoverage: 'Cobertura: Latinoamérica, el Caribe y EE.UU.',
+      planPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      deductibleMount: ['Dentro del País: USD 500', 'Fuera del País: USD 1,000'],
+      promoPercents: '15%',
+      promoPlanPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      validDate: 'Válido: febrero  1 - abril 30',
+      promoPlanPrice: 306,
+      aditionalCoverageOptions: [
+        { icon: 'medkit-outline', label: 'Transplante de órganos' },
+        { icon: 'heart-outline', label: 'Maternidad' }
+      ]
+    }
+  ];
+
+  public morePlans2 = [
+    {
+      planId: '7',
+      planTitle: '2 Bupa Advantage Care 1',
+      planSubtitle: 'Cobertura mínima',
+      planDescription: 'Este plan ofrece una cobertura básica para tu vehículo.',
+      planPrice: 120,
+      planMountLimit: 'Limite máximo: 3.5 millones',
+      planCoverage: 'Cobertura: Latinoamérica, el Caribe y EE.UU.',
+      planPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      deductibleMount: ['Dentro del País: USD 500', 'Fuera del País: USD 1,000'],
+      promoPercents: '15%',
+      promoPlanPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      validDate: 'Válido: febrero  1 - abril 30',
+      promoPlanPrice: 102,
+      aditionalCoverageOptions: [
+        { icon: 'heart-outline', label: 'Maternidad' },
+        { icon: 'airplane-outline', label: 'Piloto' }
+      ]
+    },
+    {
+      planId: '8',
+      planTitle: '2 Bupa Advantage Care 2',
+      planSubtitle: 'Cobertura mínima',
+      planDescription: 'Este plan ofrece una cobertura básica para tu vehículo.',
+      planPrice: 120.00,
+      planMountLimit: 'Limite máximo: 3.5 millones',
+      planCoverage: 'Cobertura: Latinoamérica, el Caribe y EE.UU.',
+      planPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      deductibleMount: ['Dentro del País: USD 500', 'Fuera del País: USD 1,000'],
+      promoPercents: '15%',
+      promoPlanPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      validDate: 'Válido: febrero  1 - abril 30',
+      promoPlanPrice: 102,
+      aditionalCoverageOptions: [
+        { icon: 'medkit-outline', label: 'Transplante de órganos' },
+        { icon: 'heart-outline', label: 'Maternidad' },
+        { icon: 'airplane-outline', label: 'Piloto' }
+      ]
+    },
+    {
+      planId: '9',
+      planTitle: '2 Bupa Advantage Care 3',
+      planSubtitle: 'Cobertura mínima',
+      planDescription: 'Este plan ofrece una cobertura básica para tu vehículo.',
+      planPrice: 360,
+      planMountLimit: 'Limite máximo: 3.5 millones',
+      planCoverage: 'Cobertura: Latinoamérica, el Caribe y EE.UU.',
+      planPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      deductibleMount: ['Dentro del País: USD 500', 'Fuera del País: USD 1,000'],
+      promoPercents: '15%',
+      promoPlanPaymentPlan: 'Primer pago USD 120 <br>12 pagos adicionales  USD 52',
+      validDate: 'Válido: febrero  1 - abril 30',
+      promoPlanPrice: 306,
+      aditionalCoverageOptions: [
+        { icon: 'medkit-outline', label: 'Transplante de órganos' },
+        { icon: 'heart-outline', label: 'Maternidad' }
+      ]
+    }
+  ];
+  moreSelectedOption: string | undefined = undefined;
+
   constructor() {}
 
   ngOnInit() {
@@ -91,6 +214,29 @@ export class InternationalBusinessPlansComponent  implements OnInit {
       this.selectedCount--;
     }
     console.log('Planes seleccionados '+this.selectedCount);
+  }
+
+  onMoreOptionSelected(option: string) {
+    this.moreSelectedOption = option;
+
+    // Retardar el scroll para asegurarse de que el contenido esté visible
+    setTimeout(() => {
+      this.scrollToContent();
+    }, 250);  // Puedes ajustar el tiempo si es necesario
+  }
+
+  get morePlansShowed() {
+    if (!this.moreSelectedOption) {
+      return [];  // Retorna un arreglo vacío si no se ha seleccionado ninguna opción
+    }
+    return this.moreSelectedOption === '1' ? this.morePlans1 : this.morePlans2;
+  }
+
+  scrollToContent() {
+    if (this.moreOptionsAnchor) {
+
+      this.moreOptionsAnchor.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
 }
