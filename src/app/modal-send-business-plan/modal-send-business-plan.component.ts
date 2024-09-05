310a2f830cc859b3f9fdf7280a7f6ec854c34ed9
DONNY <dvasquez@donnyvasquez.com>
2024-09-05 14:05:15 -0500

    ajustes del modal

--- src/app/modal-send-business-plan/modal-send-business-plan.component.ts
+++ src/app/modal-send-business-plan/modal-send-business-plan.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input, Output, ViewChild } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonRadioGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-send-business-plan',
@@ -17,6 +17,10 @@ export class ModalSendBusinessPlanComponent   {
   canDismiss = true;
   presentingElement: Element | null = null;
   elementId: any
+  @ViewChild('contactOptionsRadioGroup') radioGroup: IonRadioGroup | undefined;
+
+  showContactOptions: boolean = false;
+  selectedContactOption!: string | undefined;
 
   constructor() {
     this.selectedContactMethod = '';
@@ -25,6 +29,17 @@ export class ModalSendBusinessPlanComponent   {
   ngOnInit() {
     this.elementId = 'police-plan-card-'+this.planId;
     this.presentingElement = document.querySelector(this.elementId);
+  }
+
+  showContactOptionsToogle() {
+    this.showContactOptions = !this.showContactOptions;
+    if (this.selectedContactOption = 'phone') {
+      this.selectedContactOption = '';
+    }
+  }
+  showSelectedContactOptionsChange(event: CustomEvent) {
+    this.selectedContactOption = event.detail.value;
+
   }
 
 }
