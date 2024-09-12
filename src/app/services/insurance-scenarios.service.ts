8bf7e54443b0425b50b38b3ab6c650ed6ee1f2f9
DONNY <dvasquez@donnyvasquez.com>
2024-09-12 11:11:10 -0500

    Mejora del servicio para no perder el tipo de cotización

--- src/app/services/insurance-scenarios.service.ts
+++ src/app/services/insurance-scenarios.service.ts
@@ -6,29 +6,43 @@ import { Injectable } from '@angular/core';
 export class InsuranceScenariosService {
   private policeCase!: string;
 
-  // Metodo para establecer el valor de policeCase
-  setPoliceCase(value: string): void {
-    this.policeCase = value;
+  constructor() {
+    // Si hay un valor en sessionStorage, lo cargamos al inicializar el servicio
+    const storedPoliceCase = sessionStorage.getItem('policeCase');
+    if (storedPoliceCase) {
+      this.policeCase = storedPoliceCase;
+    }
   }
 
+  // Método para establecer el valor de policeCase y guardarlo en sessionStorage
+  setPoliceCase(value: string): void {
+    this.policeCase = value;
+    sessionStorage.setItem('policeCase', value);  // Guardar en sessionStorage
+  }
+
+  // Método para obtener el valor de policeCase
   getPoliceCase(): string {
+    // Si el valor no está en memoria, lo obtenemos de sessionStorage
+    if (!this.policeCase) {
+      this.policeCase = sessionStorage.getItem('policeCase') || '';
+    }
     return this.policeCase;
   }
 
-  // Metodos que devuelven true o false segun el valor de policeCase
+  // Métodos que devuelven true o false según el valor de policeCase
   isNewBusinessAgent(): boolean {
-    return this.policeCase === 'agent';
+    return this.getPoliceCase() === 'agent';
   }
 
   isNewBusinessCustomer(): boolean {
-    return this.policeCase === 'customer';
+    return this.getPoliceCase() === 'customer';
   }
 
   isRenewal(): boolean {
-    return this.policeCase === 'renewal';
+    return this.getPoliceCase() === 'renewal';
   }
 
   isPmiEcuador(): boolean {
-    return this.policeCase === 'pmiecuador';
+    return this.getPoliceCase() === 'pmiecuador';
   }
 }
