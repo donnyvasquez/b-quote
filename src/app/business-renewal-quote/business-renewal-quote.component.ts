6d1ddc609f313039296149b9ea0eaf3a8f807446
DONNY <dvasquez@donnyvasquez.com>
2024-09-26 08:19:53 -0500

    :wrench: :lipstick: Inclusión de insured card en cotizacion de renovaciones para agregar nuevas personas

--- src/app/business-renewal-quote/business-renewal-quote.component.ts
+++ src/app/business-renewal-quote/business-renewal-quote.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonModal } from '@ionic/angular';
+import { IonToast } from '@ionic/angular';
 import { ButtonActionsComponent } from "../button-actions/button-actions.component";
 import { StripHtmlPipe } from '../pipes/strip-html.pipe';
 import { RouterModule } from '@angular/router';
 import { BupaFieldOutputComponent } from "../bupa-field-output/bupa-field-output.component";
 import { InsuredPersonDetails, RenewalInsuredPersonComponent } from "../renewal-insured-person/renewal-insured-person.component";
+import { InsuredBusinessCardComponent } from "../insured-business-card/insured-business-card.component";
 
 @Component({
   selector: 'app-business-renewal-quote',
   templateUrl: './business-renewal-quote.component.html',
   styleUrls: ['./business-renewal-quote.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule, IonicModule, StripHtmlPipe, ButtonActionsComponent, BupaFieldOutputComponent, RenewalInsuredPersonComponent]
+  imports: [CommonModule, RouterModule, IonicModule, StripHtmlPipe, ButtonActionsComponent, BupaFieldOutputComponent, RenewalInsuredPersonComponent, InsuredBusinessCardComponent]
 })
 export class BusinessRenewalQuoteComponent implements OnInit {
-
+  @ViewChild('modal', { static: true }) modal!: IonModal;
+  @ViewChild('toast', { static: false }) toast!: IonToast;
+  relationshipOptions = [
+    { value: 'spouse', label: 'Spouse' }, // Esposo/a
+    { value: 'child', label: 'Child' },   // Hijo/a
+    { value: 'parent', label: 'Parent' }, // Padre/Madre
+    { value: 'sibling', label: 'Sibling' }, // Hermano/a
+    { value: 'other', label: 'Other' }    // Otro
+  ];
   insuredPeople: InsuredPersonDetails[] = [
     {
       title: 'Titular',
       details: {
-        inPolice: true,  // De la póliza
-        enabledInQuote: true,  // Activo en la cotización
+        inPolice: true, // De la póliza o nuevo en la cotizacion
+        enabledInQuote: true, // Activo en la cotización
         typeLabel: 'Titular/Guardián',
         typeValue: 'María Pérez Puertos',
         birthDateLabel: 'Fecha nacimiento',
@@ -29,14 +39,16 @@ export class BusinessRenewalQuoteComponent implements OnInit {
         rateLabel: 'Tarifa',
         rateValue: 'USD 7,235.00',
         extraPremiumLabel: 'Extraprima',
-        extraPremiumValue: '10% (USD 7,958.50)'
+        extraPremiumValue: '10% (USD 7,958.50)',
+        isOverDate: undefined,
+        insuredType: 'titular'
       }
     },
     {
       title: 'Cónyuge',
       details: {
-        inPolice: true,  // De la póliza
-        enabledInQuote: true,  // Activo en la cotización
+        inPolice: true, // De la póliza
+        enabledInQuote: true, // Activo en la cotización
         typeLabel: 'Dependiente',
         typeValue: 'José Adrián Pérez Puertos',
         birthDateLabel: 'Fecha nacimiento',
@@ -44,22 +56,43 @@ export class BusinessRenewalQuoteComponent implements OnInit {
         rateLabel: 'Tarifa',
         rateValue: 'USD 8,950.00',
         extraPremiumLabel: 'Extraprima',
-        extraPremiumValue: ''
+        extraPremiumValue: '',
+        isOverDate: undefined,
+        insuredType: 'spouse'
       }
     },
     {
       title: 'Dependiente 2',
       details: {
-        inPolice: true,  // De la póliza
-        enabledInQuote: true,  // Activo en la cotización
+        insuredType: 'dependent',
+        inPolice: true, // De la póliza
+        enabledInQuote: true, // Activo en la cotización
         typeLabel: 'Dependiente',
         typeValue: 'Sara Adriana Pérez',
         birthDateLabel: 'Fecha nacimiento',
-        birthDateValue: '03-03-2010',
+        birthDateValue: '03-03-2000',
         rateLabel: 'Tarifa',
         rateValue: 'USD 1,300.00',
         extraPremiumLabel: 'Extraprima',
-        extraPremiumValue: ''
+        extraPremiumValue: '',
+        isOverDate: true
+      }
+    },
+    {
+      title: 'Dependiente 3',
+      details: {
+        insuredType: 'dependent',
+        inPolice: true, // De la póliza
+        enabledInQuote: true, // Activo en la cotización
+        typeLabel: 'Dependiente',
+        typeValue: 'Eilier Vásquez Paternina',
+        birthDateLabel: 'Fecha nacimiento',
+        birthDateValue: '03-03-2021',
+        rateLabel: 'Tarifa',
+        rateValue: 'USD 1,300.00',
+        extraPremiumLabel: 'Extraprima',
+        extraPremiumValue: '',
+        isOverDate: false
       }
     }
   ];
@@ -137,15 +170,74 @@ export class BusinessRenewalQuoteComponent implements OnInit {
   activeInsuredPeople: InsuredPersonDetails[] = [];
   disabledInsuredPeople: InsuredPersonDetails[] = [];
   quoteIsDiffPolice: boolean = false;
+
+  costosCTAsData = {
+    titulo: "Costo",
+    columnas: {
+      subtotal: [
+        { label: "Subtotal", valor: "USD 18,208.50" },
+        { label: "Costo administrativo Anual", valor: "USD 86" },
+        { label: "IVA (14.94%)", valor: "USD 2,584.33" }
+      ],
+      discounts: [
+        { label: "Descuento por póliza (5%)", valor: "USD 300" },
+        { label: "Extraprima (10%)", valor: "USD 2,350" },
+        { label: "Descuento ONA (4%)", valor: "USD 435" }
+      ],
+      totals: {
+        items: [
+          { label: "Total", valor: "USD 19,968.40" },
+          { label: "Primer pago", valor: "USD 143.60" },
+          { label: "Pagos 2-12", valor: "USD 68.60" }
+        ],
+        nota: "Costo no incluye dependiente mayor de 24 años, quién debe obtener su propia póliza."
+      }
+    }
+  };
+
+  presentingElement = document.querySelector('.main-content-renewal');
+
   ngOnInit() {
     this.filterInsuredPeople();
   }
 
+
+
+  calculateAge(birthDate: string): number {
+    const birth = new Date(birthDate);
+    const today = new Date();
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDiff = today.getMonth() - birth.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+      age--;
+    }
+
+    return age;
+  }
+
+
   // Método para filtrar los asegurados activos y desactivados
   filterInsuredPeople() {
+    this.insuredPeople.forEach(person => {
+      const age = this.calculateAge(person.details.birthDateValue);
+      console.log(`Person: ${person.details.typeValue}, Age: ${age}`); // Para verificar la edad calculada
+
+      // Desactivar si la edad es 24 o más
+      if (age >= 24 && person.details.insuredType === 'dependent') {
+        person.details.enabledInQuote = false;
+        person.details.isOverDate = true;
+      }
+    });
+
+    // Filtrar asegurados activos y desactivados
     this.activeInsuredPeople = this.insuredPeople.filter(person => person.details.enabledInQuote);
     this.disabledInsuredPeople = this.insuredPeople.filter(person => !person.details.enabledInQuote);
+
+    console.log('Active insured:', this.activeInsuredPeople); // Para verificar el filtrado de asegurados activos
+    console.log('Disabled insured:', this.disabledInsuredPeople); // Para verificar el filtrado de asegurados desactivados
   }
+
 
   comparePoliceQuote():boolean{
     return this.activeInsuredPeople == this.insuredPeople;
@@ -192,7 +284,7 @@ export class BusinessRenewalQuoteComponent implements OnInit {
 
     if (hasChanges) {
       sessionStorage.setItem('pendingChanges', JSON.stringify(activeInsuredPeople));
-      alert("Hay cambios pendientes de guardar.");
+      this.presentToast();
       this.quoteIsDiffPolice = true;
     } else {
       sessionStorage.removeItem('pendingChanges');
@@ -200,6 +292,33 @@ export class BusinessRenewalQuoteComponent implements OnInit {
     }
   }
 
+  async presentToast() {
+    await this.toast.present();
+  }
+
+  addNewInsured() {
+    this.insuredPeople.push({
+      title: 'Dependiente '+this.insuredPeople.length,
+      details: {
+        insuredType: 'dependent',
+        inPolice: false,
+        enabledInQuote: true, // Activo en la cotización
+        typeLabel: 'Dependiente',
+        typeValue: 'Darleen Vásquez Paternina',
+        birthDateLabel: 'Fecha nacimiento',
+        birthDateValue: '02-22-2001',
+        rateLabel: 'Tarifa',
+        rateValue: 'USD 1,400.00',
+        extraPremiumLabel: 'Extraprima',
+        extraPremiumValue: '10% (USD 7,958.50)',
+        isOverDate: undefined
+      }
+    });
+    this.filterInsuredPeople()
+    //this.disabledInsuredPeople = this.insuredPeople.filter(person => !person.details.enabledInQuote);
+    this.checkForChangesAndSave(this.insuredPeople,this.activeInsuredPeople)
+    this.modal.dismiss()
+  };
   // Método resetQuote dentro de la clase
   resetQuote(insuredPeople: any[], activeInsuredPeople: any[], disabledInsuredPeople: any[]) {
     activeInsuredPeople.length = 0;
